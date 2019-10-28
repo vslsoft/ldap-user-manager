c5d9718458a7cf86ed53c2a42d3f95bb56439446
Dzung Truong <dung.truong@gmail.com>
2019-10-28 15:39:14 +0700

    Rework CSP

--- www/includes/web_functions.inc.php
+++ www/includes/web_functions.inc.php
@@ -170,11 +170,10 @@ function render_header($title="",$menu=TRUE) {
  <meta name="viewport" content="width=device-width, initial-scale=1">
 
  <meta http-equiv="content-type" content="text/html; charset=utf-8 ;">
- <meta http-equiv="Content-Security-Policy" content="style-src 'self' https://maxcdn.bootstrapcdn.com">
- <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://maxcdn.bootstrapcdn.com">
- <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://ajax.googleapis.com">
- <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://cdnjs.cloudflare.com">
-:
+ <meta http-equiv="Content-Security-Policy" content="style-src *">
+ <meta http-equiv="Content-Security-Policy" content="script-src *">
+
+
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
