90be7e0a026ca0a527628a0ccee52498b3300720
Dzung Truong <dung.truong@gmail.com>
2019-10-28 15:10:55 +0700

    Re-work on CSP issues

--- www/includes/web_functions.inc.php
+++ www/includes/web_functions.inc.php
@@ -170,12 +170,12 @@ function render_header($title="",$menu=TRUE) {
  <meta name="viewport" content="width=device-width, initial-scale=1">
 
  <meta http-equiv="content-type" content="text/html; charset=utf-8 ;">
- <meta http-equiv="Content-Security-Policy" content="style-src 'self' maxcdn.bootstrapcdn.com">
- <meta http-equiv="Content-Security-Policy" content="script-src 'self' maxcdn.bootstrapcdn.com">
- <meta http-equiv="Content-Security-Policy" content="script-src 'self' ajax.googleapis.com">
+ <meta http-equiv="Content-Security-Policy" content="style-src 'self' https://maxcdn.bootstrapcdn.com">
+ <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://maxcdn.bootstrapcdn.com">
+ <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://ajax.googleapis.com">
  <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://cdnjs.cloudflare.com">
-
- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+:
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </HEAD>
