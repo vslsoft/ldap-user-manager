66492f40339ec61b84f6e1ed708718d2afa680d9
Dzung Truong <dung.truong@gmail.com>
2019-10-28 14:46:31 +0700

    Fixed Content-Security-Policy issues

--- www/includes/web_functions.inc.php
+++ www/includes/web_functions.inc.php
@@ -168,6 +168,13 @@ function render_header($title="",$menu=TRUE) {
  <TITLE><?php print "$title"; ?></TITLE>
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <meta http-equiv="content-type" content="text/html; charset=utf-8 ;">
+ <meta http-equiv="Content-Security-Policy" content="style-src 'self' maxcdn.bootstrapcdn.com">
+ <meta http-equiv="Content-Security-Policy" content="script-src 'self' maxcdn.bootstrapcdn.com">
+ <meta http-equiv="Content-Security-Policy" content="script-src 'self' ajax.googleapis.com">
+ <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://cdnjs.cloudflare.com">
+
  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
